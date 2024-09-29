62d5084d4d30bcf00fb9ef9fefe95d6f2e8d8443
Carlos Cogollo <carloscogoza@gmail.com>
2024-09-29 16:57:27 +0000

    Initial commit

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// New API endpoint for /api/whoami
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip; // Get IP address
+  const language = req.headers['accept-language']; // Get preferred language
+  const software = req.headers['user-agent']; // Get software information
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
